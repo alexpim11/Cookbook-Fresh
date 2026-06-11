c40fa156cdf59509cfd07ea3d84b1c0957ab833a
Alex <h22alex@gmail.com>
2026-06-11 18:33:12 -0400

    iOS polish: compact bottom bar, no input zoom, modal text wrapping

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Caches the app shell so it works offline once installed.
  * Bump CACHE_VERSION whenever you change the HTML and want users to get the update.
  */
-const CACHE_VERSION = 'cookbook-v18-households';
+const CACHE_VERSION = 'cookbook-v19-ios-polish';
 const ASSETS = [
   './',
   './cookbook-fresh.html',
