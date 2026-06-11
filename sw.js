19c9e7431e2bb9197281cae1c6380e960c803069
Alex <h22alex@gmail.com>
2026-06-11 10:03:01 -0400

    Rename app to Stir Crazy

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Caches the app shell so it works offline once installed.
  * Bump CACHE_VERSION whenever you change the HTML and want users to get the update.
  */
-const CACHE_VERSION = 'cookbook-v15-auth-debug';
+const CACHE_VERSION = 'cookbook-v16-stircrazy';
 const ASSETS = [
   './',
   './cookbook-fresh.html',
