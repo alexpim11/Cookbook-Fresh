18e76fffb1ee53a257ef17d554ede5a9b649396c
Alex <h22alex@gmail.com>
2026-06-11 18:41:41 -0400

    Modal flex fixes: shrinkable inputs, pinned Join button

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Caches the app shell so it works offline once installed.
  * Bump CACHE_VERSION whenever you change the HTML and want users to get the update.
  */
-const CACHE_VERSION = 'cookbook-v19-ios-polish';
+const CACHE_VERSION = 'cookbook-v20-modal-flex';
 const ASSETS = [
   './',
   './cookbook-fresh.html',
