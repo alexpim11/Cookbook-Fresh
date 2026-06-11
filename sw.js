c0e5c3b6a43f1e0631a6a92e73ffe4a86f63a2a8
Alex <h22alex@gmail.com>
2026-06-11 18:15:05 -0400

    No auto-join on sign-in; add household switching by code

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Caches the app shell so it works offline once installed.
  * Bump CACHE_VERSION whenever you change the HTML and want users to get the update.
  */
-const CACHE_VERSION = 'cookbook-v17-viewport';
+const CACHE_VERSION = 'cookbook-v18-households';
 const ASSETS = [
   './',
   './cookbook-fresh.html',
