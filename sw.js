86ba7c431e9071d4c4383ea0fc793217fc2a73b3
Alex <h22alex@gmail.com>
2026-06-14 07:59:50 -0400

    Preserve source step grouping: split instructions on newlines before collapsing whitespace

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Caches the app shell so it works offline once installed.
  * Bump CACHE_VERSION whenever you change the HTML and want users to get the update.
  */
-const CACHE_VERSION = 'cookbook-v22-steps-source';
+const CACHE_VERSION = 'cookbook-v23-step-grouping';
 const ASSETS = [
   './',
   './cookbook-fresh.html',
